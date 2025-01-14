e16d08a1a58c35ec4cb2ef0a214d321028ea0ab5
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-14 13:57:03 +0530

    Set Up Auth Controllers

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -3,10 +3,13 @@ import { signup, login, logout } from "../controllers/auth.controllers.js";
 
 const router = express.Router();
 
+//Signup route
 router.post("/signup", signup);
 
+//login route
 router.post("/login", login);
 
+//logout route
 router.post("/logout", logout);
 
 export default router;
