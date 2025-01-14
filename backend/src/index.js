9ecc44fafbf11854f9a91102b3e93f412d26db67
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-14 18:23:39 +0530

    Backend Setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,25 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
+//Dotenv Config:
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT;
 
+//Middlewares:
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth", authRoutes);
 
+//Routes:
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+//Listening of PORT :
 app.listen(PORT, () => {
   console.log(`The server is running on port : ${PORT}`);
   connectDB();
---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+import {
+  getUsersForSidebar,
+  getMessages,
+  sendMessage,
+} from "../controllers/message.controller.js";
+
+const router = express.Router();
+
+//getting users for sidebar menu :
+router.get("/users", protectRoute, getUsersForSidebar);
+
+//getting messages latest order :
+router.get("/:id", protectRoute, getMessages);
+
+//sending messages :
+router.post("/send/:id", protectRoute, sendMessage);
+export default router;
